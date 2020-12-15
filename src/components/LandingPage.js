8988abc270467982712490c820d89b41ea2384f4
Derek Smale <dereksmale2@gmail.com>
2020-12-15 14:29:09 -0500

    fixed landing page issue

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -4,12 +4,10 @@ import { Link } from 'react-router-dom'
 const LandingPage = () => {
   return (
     <div className='LandingPage'>
-      <h1>
-        <Link to='/foster-family-register'>Register</Link> /{' '}
-        <Link to='/foster-family-login'>Login</Link> as a Foster Family or{' '}
-        <Link to='/donor-register'>Register</Link> /{' '}
-        <Link to='/donor-login'>Login</Link> as a Donor
-      </h1>
+      <Link to='/foster-family-signup'>Register</Link> /{' '}
+      <Link to='/foster-family-login'>Login</Link> as a Foster Family or{' '}
+      <Link to='/donor-signup'>Register</Link> /{' '}
+      <Link to='/donor-login'>Login</Link> as a Donor
     </div>
   )
 }
