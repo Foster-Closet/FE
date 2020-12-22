5062e2aa88a6c633ef78089a6bb2a49216bcf6c1
Kim Leibowitz <kim@brightlightwellness.com>
2020-12-21 19:18:29 -0500

    ffDashboard Reuested list map

--- src/components/FFDashBoard.js
+++ src/components/FFDashBoard.js
@@ -19,12 +19,33 @@ const FFDashboard = ({ auth }) => {
     return <Redirect to='/foster-family-login' />
   }
 
+
   return (
     <div className='FFDashboard'>
+      
       <h1>Helping our community</h1>
       <a href='/create-request'>Create a Request</a>
-      {requestList.map((item) => console.log(item))}
-    </div>
+      <div>
+        <h1>My Requested List</h1>
+        {
+          requestList.map((item) => 
+              <div>Item:{item.id}
+              <ul>
+                {item.items.map((sub)=>
+                <li>
+                  {sub.description}
+                </li>
+                  )}
+              </ul>
+              </div>
+          )
+        }
+        </div>
+        )
+      }
+    
+
+      </div>
   )
 }
 
