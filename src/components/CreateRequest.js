e4ffaffd848868278ec2cffd37b30031aededfce
Derek Smale <dereksmale2@gmail.com>
2020-12-22 10:35:41 -0500

    added material-ui component, in testing

--- src/components/CreateRequest.js
+++ src/components/CreateRequest.js
@@ -1,16 +1,16 @@
 import React, { useState } from 'react'
 import axios from 'axios'
 import { Redirect } from 'react-router-dom'
+import TransferList from './TravelEquipment'
 
 const CreateRequest = ({ auth }) => {
-  const [itemInList, setItemInList] = useState(null)
   const [submitted, setSubmitted] = useState(false)
 
   const handleSubmit = () => {
     axios
       .post(
         'https://foster-closet.herokuapp.com/api/registry/',
-        { items: [{ description: itemInList }] },
+        { items: [{ description: TransferList }] },
         { headers: { Authorization: `Token ${auth}` } }
       )
       .then((response) => {
@@ -26,30 +26,7 @@ const CreateRequest = ({ auth }) => {
     <div className='CreateRequest'>
       <h2>Create a request for you foster child here</h2>
       <div>
-        <label>
-          <input
-            type='checkbox'
-            value='stroller'
-            checked={itemInList === 'stroller'}
-            onChange={(e) => {
-              setItemInList(e.target.value)
-            }}
-          />
-          Stroller
-        </label>
-      </div>
-      <div>
-        <label>
-          <input
-            type='checkbox'
-            value='car seat'
-            checked={itemInList === 'car seat'}
-            onChange={(e) => {
-              setItemInList(e.target.value)
-            }}
-          />
-          Car Seat
-        </label>
+        <TransferList />
       </div>
       <button onClick={handleSubmit}>Submit Request</button>
     </div>
